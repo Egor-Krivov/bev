8da61235d32cd6fd57a361ec3821939f52ff3cd9
andrey <shushko.ai@phystech.edu>
2022-02-10 16:15:39 +0300

    replace argparse cli with typer

--- bev/cli/entrypoint.py
+++ bev/cli/entrypoint.py
@@ -1,3 +1,5 @@
+import typer
+from typint import List
 from argparse import ArgumentParser
 
 from .add import add
@@ -8,58 +10,73 @@ from .pull import pull, gather, PULL_MODES
 from .storage import init_storage
 from .update import update
 
+app = typer.typer()
+
+
+@app.command(name="init")
+def _init(
+        repository: str = typer.Argument('.'),
+        permissions: str = typer.Option(None, "--permissions", "-p"),
+        group: str = typer.Option(None, "--group", "-g")
+        ):
+    init(repository, permissions, group)
+
+
+@app.command(name="add")
+def _add(
+        source: str = typer.Argument(..., help='a list of files/folders to be added'),
+        destination: str = typer.Argument(..., help='the final location of the files/folders'),
+        keep: bool = typer.Option(False, "--keep", "-k"),
+        context: str = typer.Option(None, "--context", "-c")
+        ):
+    add(source, destination, keep, context)
+
+
+@app.command(name="fetch")
+def _fetch(
+        paths: List[str] = typer.Argument(...),
+        context: str = typer.Option(None, "--context", "-c")
+        ):
+    fetch(paths, context)
+
+
+@app.command(name="pull")
+def _pull(
+        source: str = typer.Argument(...),
+        destination: str = typer.Argument(...),
+        mode: str = typer.Option(..., "--mode", "-m", prompt=True, help='how to pull the files from the hash.')
+        ):
+    pull(source, destination, mode)
+
+
+@app.command(name="gather")
+def _gather(
+        source: str = typer.Argument(...),
+        destination: str = typer.Argument(...),
+        ):
+    gather(source, destination)
+
+
+@app.command(name="update")
+def _update(
+        source: str = typer.Argument(...),
+        destination: str = typer.Argument(...),
+        keep: bool = typer.Option(False, "--keep", "-k"),
+        overwrite: bool = typer.Option(False, "--overwrite", "-o")
+        ):
+    update(source, destination, keep, overwrite)
+
+
+@app.command(name="blame")
+def _blame(
+        path: str = typer.Argument(...),
+        relative: str = typer.Argument(...),
+        ):
+    blame(path, relative)
+
 
 def entrypoint():
-    parser = ArgumentParser()
-    subparsers = parser.add_subparsers()
-
-    new = subparsers.add_parser('init')
-    new.set_defaults(callback=init)
-    new.add_argument('repository', default='.', nargs='?')
-    new.add_argument('-p', '--permissions')
-    new.add_argument('-g', '--group')
-
-    new = subparsers.add_parser('add')
-    new.set_defaults(callback=add)
-    new.add_argument('source')
-    new.add_argument('destination')
-    new.add_argument('-k', '--keep', default=False, action='store_true')
-
-    new = subparsers.add_parser('fetch')
-    new.set_defaults(callback=fetch)
-    new.add_argument('paths', nargs='+')
-
-    new = subparsers.add_parser('pull')
-    new.set_defaults(callback=pull)
-    new.add_argument('source')
-    new.add_argument('destination')
-    new.add_argument('--mode', choices=list(PULL_MODES), help='how to pull the files from the hash.')
-
-    new = subparsers.add_parser('gather')
-    new.set_defaults(callback=gather)
-    new.add_argument('source')
-    new.add_argument('destination')
-
-    new = subparsers.add_parser('update')
-    new.set_defaults(callback=update)
-    new.add_argument('source')
-    new.add_argument('destination')
-    new.add_argument('-k', '--keep', default=False, action='store_true')
-    new.add_argument('--overwrite', default=False, action='store_true')
-
-    new = subparsers.add_parser('blame')
-    new.set_defaults(callback=blame)
-    new.add_argument('path')
-    new.add_argument('relative')
-
-    # add_storage_functions(subparsers.add_parser('storage'))
-
-    args = vars(parser.parse_args())
-    if 'callback' not in args:
-        parser.print_help()
-    else:
-        callback = args.pop('callback')
-        callback(**args)
+    app()
 
 
 def add_storage_functions(parser: ArgumentParser):
